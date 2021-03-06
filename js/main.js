db9db3cd43ab5b4a75624376cd4a34ec6914df7b
José Man <34019072+JoseMan200124@users.noreply.github.com>
2021-03-06 01:09:00 -0600

    Actualización carrusel
    carrusel de imagenes responsive

--- js/main.js
+++ js/main.js
@@ -1,71 +1,92 @@
-let productIndex = 0
+let isScroll = false
 
-let productInfos = document.querySelectorAll('.product-info')
+scrollSlide = () => {
 
-setTimeout(() =>{
-    productInfos[productIndex].classList.add('active');
-}, 200);
+    if (isScroll) return
 
-let isSliding = false
+    isScroll = true
 
-slide=()=>{
-    if(isSliding) return
-    isSliding = true
     let currProduct = document.querySelector('.product-info.active')
     currProduct.classList.remove('active')
     productIndex = productIndex + 1 > productInfos.length - 1 ? 0 : productIndex + 1
     productInfos[productIndex].classList.add('active')
 
+    let  listitems = document.querySelectorAll('.slide')
+
+    let slider = document.querySelector('.slider')
+
+    let reverse = Array.from(listitems).slice().reverse()
+
+    left = reverse[0].offsetLeft+'px'
+    height = reverse[0].offsetHeight+'px'
+    width = reverse[0].offsetWidth+'px'
+    zIndex = reverse[0].style.zIndex
+
+    reverse.forEach((el, index) => {
     
-    let listItems = document.querySelectorAll('.slide')
-    let slider = document.querySelectorAll('.slider')
-    
-    let reverseItems = Array.from(listItems).slice().reverse()
-    
-    left = reverseItems[0].offsetLeft + 'px'
-    height = reverseItems[0].offsetHeight + 'px'
-    width = reverseItems[0].offsetWidth + 'px' 
-        zIndex = reverseItems[0].style.zIndex 
-    
-    reverseItems.forEach((e1, index)=>{
-        if(index < listItems.length - 1){
-            e1.style.left = reverseItems[index + 1].offsetLeft + 'px'
-           
-            e1.style.height = reverseItems[index + 1].offsetHeight + 'px'
-            
-            e1.style.width = reverseItems[index + 1].offsetWidth+ 'px'
-            
-            e1.style.zIndex = reverseItems[index + 1].style.zIndex
-            
-            e1.style.transform = 'unset'
-            e1.style.opacity = 1
-            
+        if (index < listitems.length-1) {
+            el.style.left = reverse[index + 1].offsetLeft+'px'
+            el.style.height = reverse[index + 1].offsetHeight+'px'
+            el.style.width = reverse[index + 1].offsetWidth+'px'
+            el.style.zIndex = index + 1
+            el.style.transform = 'unset'
+            el.style.opacity = '1'
         }
-        if(index == listItems.length - 1){
-            e1.style.transform = 'scale(1.5)'
-            e1.style.opacity = '0'
+        if (index === listitems.length - 1) {
+            el.style.transform = 'scale(1.5)'
+            el.style.opacity = '0'
+
+            let cln = el.cloneNode(true)
             
-            let cln = e1.cloneNode(true)
-            setTimeout(()=>{
-             e1.remove()
+            setTimeout(() => {
+                el.remove()
                 cln.style.transform = 'scale(0)'
                 cln.style.left = left
                 cln.style.height = height
                 cln.style.width = width
+                // cln.style.transform = 'unset'
                 cln.style.opacity = '0'
-                cln.style.zIndex = '0'
+                cln.style.zIndex = 0
                 cln.style.animation = 'unset'
-                
                 slider.appendChild(cln)
-                isSliding = false
+                isScroll = false
             }, 1000);
         }
     })
-    
+    listitems = document.querySelectorAll('.slide')
+    listitems[0].style.zIndex = '4'
 }
+
+let productIndex = 0
+
+let productInfos = document.querySelectorAll('.product-info')
+
+setTimeout(() => {
+    productInfos[productIndex].classList.add('active')
+}, 200);
+
 let slideControl = document.querySelector('.slide-control')
-slideControl.onclick = () =>{
-   slide() 
+
+slideControl.onclick = (e) => {
+    scrollSlide()
 }
 
+openNav = () => {
+    let nav = document.querySelector('.nav-overlay')
+    let hamb = document.querySelector('.hamburger')
+    nav.classList.toggle('active')
+    hamb.classList.toggle('active')
+}
 
+// let hambBtn = document.querySelector('.hamburger-btn')
+
+// let hamb = hambBtn.querySelector('.hamburger')
+
+// hambBtn.onclick = () => {
+//     hamb.classList.toggle('active')
+// }
+
+// scrollSlide()
+// setInterval(() => {
+//     scrollSlide()
+// }, 2200);
